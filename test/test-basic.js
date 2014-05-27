dfa66892c9664b1d34de90813f399309e2b3e03c
Alex Gorrod <alexg@wiredtiger.com>
2014-05-27 11:07:49 +1000

    Restructure test code to limit concurrency.

--- test/test-basic.js
+++ test/test-basic.js
@@ -1,7 +1,9 @@
 var wiredtiger = require('../'),
      _ = require('underscore');
 
-const numPuts = 5000;
+const concurrency = 50;
+const numPuts = 500000;
+const numPutsPerThread = numPuts / concurrency;
 
 var conn = new wiredtiger.WTConnection(
     '/tmp/test', 'create,async=(enabled=true,ops_max=4096)');
@@ -11,32 +13,60 @@ conn.Open( function(err) {
 
 	var didPut = _.after(numPuts, afterPuts);
 	var didGet = _.after(numPuts, afterGets);
+	var totalWrites = 0;
+	var totalSearches = 0;
 	var table = new wiredtiger.WTTable(
 	    conn, 'table:test', 'create,key_format=S,value_format=S');
+
+	function doPut (threadNum, itemNum) {
+		if (totalWrites == numPuts) {
+			console.log("Finished " + numPuts + " writes");
+			return
+		}
+		if (itemNum >= numPutsPerThread)
+			return
+		var keyOffset = (threadNum * numPutsPerThread) + itemNum;
+		table.Put('abc' + keyOffset, 'def' + keyOffset, function(err) {
+			if (err)
+				throw err
+			//console.log('Did put: ' + threadNum + ':' + itemNum);
+			didPut();
+			totalWrites++;
+			itemNum++;
+			process.nextTick(function() {
+				doPut(threadNum, itemNum) })
+		});
+	}
 	table.Open( function(err) {
 		if (err)
 			throw err
-		var inserted = 0;
-		for (var i = 0; i < numPuts; i++) {
-			table.Put('abc' + i, 'def' + i, function(err) {
-				if (err)
-					throw err
-				didPut();
-				//console.log("Put into table");
-			});
-		}
+		for (var i = 0; i < concurrency; i++)
+			doPut(i, 0);
 	});
 
+	function doGet (threadNum, itemNum) {
+		if (totalSearches++ == numPuts) {
+			console.log("Finished " + numPuts + " searches");
+			return
+		}
+		if (itemNum >= numPutsPerThread)
+			return
+		var keyOffset = (threadNum * numPutsPerThread) + itemNum;
+		table.Search('abc' + keyOffset, function(err, result) {
+			if (err)
+				throw err
+			//console.log('Did put: ' + threadNum + ':' + itemNum);
+			didGet();
+			totalSearches++;
+			itemNum++;
+			process.nextTick(function() {
+				doGet(threadNum, itemNum) })
+		});
+	}
 	function afterPuts() {
 		console.log("Finished puts! Yay!");
-		for (var i = 0; i < numPuts; i++) {
-			//console.log('About to get');
-			table.Search('abc' + i, function(err, result) {
-				if (err)
-					throw err
-				//console.log("Got from table " + result);
-				didGet();
-			});
+		for (var i = 0; i < concurrency; i++) {
+			doGet(i, 0);
 		}
 	}
 });
